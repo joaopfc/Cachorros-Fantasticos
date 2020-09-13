a2d2e4e695ac67d1971d92745197a615d8df330f
joaopfc <joaopaulofariadecarvalho@gmail.com>
2020-09-13 17:38:24 -0300

    funcionamento refatorado

--- JS/modules/funcionamento.js
+++ JS/modules/funcionamento.js
@@ -1,21 +1,40 @@
-export default function initFuncionamento(){
-
-    const fuincionamento = document.querySelector('[data-semana]');
-    const diasSemana = fuincionamento.dataset.semana.split(',').map(Number)
-    const horarioSemana = fuincionamento.dataset.horario.split(',').map(Number)
-
-    const dataAgora = new Date();
-    const diaAgora = dataAgora.getDay();
-    const horarioAgora = dataAgora.getHours();
-
-    const semanaAberto = diasSemana.indexOf(diaAgora) !== -1
-
-    const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]);
-
-    if(semanaAberto && horarioAberto){
-        fuincionamento.classList.add('aberto');
+export default class Funcionamento {
+    constructor(functionamento, activeClass) {
+      this.funcionamento = document.querySelector(functionamento);
+      this.activeClass = activeClass;
     }
-
-} 
-
-
+  
+    dadosFuncionamento() {
+      this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number);
+      this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number);
+    }
+  
+    dadosAgora() {
+      this.dataAgora = new Date();
+      this.diaAgora = this.dataAgora.getDay();
+      this.horarioAgora = this.dataAgora.getUTCHours() - 3;
+    }
+  
+    estaAberto() {
+      const semanaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1;
+      const horarioAberto = (this.horarioAgora >= this.horarioSemana[0] && this.horarioAgora < this.horarioSemana[1]);
+      
+      return semanaAberto && horarioAberto;
+    }
+  
+    ativaAberto() {
+      if (this.estaAberto()) {
+        this.funcionamento.classList.add(this.activeClass);
+      }
+    }
+  
+    init() {
+      if (this.funcionamento) {
+        this.dadosFuncionamento();
+        this.dadosAgora();
+        this.ativaAberto();
+      }
+      return this;
+    }
+  }
+  
