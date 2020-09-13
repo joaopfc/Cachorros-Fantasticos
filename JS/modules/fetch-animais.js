47b563203bee44b41e5708f1156343a1ec335c77
joaopfc <joaopaulofariadecarvalho@gmail.com>
2020-09-12 23:39:41 -0300

    Anima numeros refatorado

--- JS/modules/fetch-animais.js
+++ JS/modules/fetch-animais.js
@@ -1,7 +1,17 @@
-import initAnimaNumeros from './anima-numeros.js'
+import AnimaNumeros from './anima-numeros.js'
 
 export default function initFetchAnimais(){
   
+    
+    
+    function createAnimal(animal){
+        const div = document.createElement('div');
+        div.classList.add('numero-animal')
+        div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`
+    
+        return div
+    }
+
     async function fetchAnimais(url){
         try{
             const animaisResponse = await fetch(url);
@@ -12,18 +22,11 @@ export default function initFetchAnimais(){
                 const divAnimal =  createAnimal(animal);
                 numeroGrid.appendChild(divAnimal)
             });
-            initAnimaNumeros();
+            const animaNumeros = new AnimaNumeros('[data-numero]','.numeros', 'ativo');
+            animaNumeros.init()
         }catch(erro){
             console.log(Error(erro))
         }
-    }
-    
-    function createAnimal(animal){
-        const div = document.createElement('div');
-        div.classList.add('numero-animal')
-        div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`
-    
-        return div
     }
     
     fetchAnimais('./animaisApi.json')
